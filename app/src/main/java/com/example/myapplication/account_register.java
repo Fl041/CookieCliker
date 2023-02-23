ccae8fa3d542e6934128df9543d47fa3f6dd160f
Fl041 <92439423+Fl041@users.noreply.github.com>
2023-02-23 18:45:40 +0100

    account show + photo qui fonctionne

--- app/src/main/java/com/example/myapplication/account_register.java
+++ app/src/main/java/com/example/myapplication/account_register.java
@@ -9,10 +9,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,8 +24,10 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
+
 public class account_register<selectedImg> extends AppCompatActivity {
-Uri selectedImg;
+Bitmap bit ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,7 +51,7 @@ Uri selectedImg;
                 String Username = username.getText().toString();
                 String Email = email.getText().toString();
                 String Password = password.getText().toString();
-                Uri avatar = selectedImg;
+                String avatar = bitmaptoString(bit);
                 Log.d("TAG", String.valueOf(avatar));
                 if(!isEmailValid(Email)) {
                     toast("Email");
@@ -92,11 +97,20 @@ Uri selectedImg;
         super.onActivityResult(requestCode, resultCode, data);
 
         if(resultCode == RESULT_OK && data != null) {
-            selectedImg = data.getData();
+            Uri selectedImg = data.getData();
             ImageView imageView = findViewById(R.id.avatarImg);
             imageView.setImageURI(selectedImg);
+            bit = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
+
         }
     }
+    private String bitmaptoString(Bitmap bit){
+        ByteArrayOutputStream stream = new ByteArrayOutputStream() ;
+        bit.compress(Bitmap.CompressFormat.PNG , 50 , stream) ;
+        byte[] byte_Array = stream.toByteArray();
+        return Base64.encodeToString(byte_Array , 0);
+    }
+
 
     boolean isEmailValid(CharSequence email) {
         return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
@@ -106,8 +120,8 @@ Uri selectedImg;
         return !text.equals("");
     }
 
-    boolean ImageValid(Uri avatar){
-        return !(String.valueOf(avatar).equals("null"));
+    boolean ImageValid(String avatar){
+        return !avatar.equals("");
     }
     void toast(String champs) {
         if(champs.equals("Email")) {
--- app/src/main/java/com/example/myapplication/account_show.java
+++ app/src/main/java/com/example/myapplication/account_show.java
@@ -1,8 +1,19 @@
 package com.example.myapplication;
 
+import static com.example.myapplication.AccountDBHelper.BASE_NOM;
+import static com.example.myapplication.AccountDBHelper.BASE_VERSION;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Base64;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,11 +23,15 @@ public class account_show extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_show);
-
+        AccountDBHelper dbHelper = new AccountDBHelper(this, BASE_NOM, null, BASE_VERSION);
         // Récupérer les informations de l'utilisateur dans la BDD
-        String username = "Username : test";
-        String email = "E-mail : test@gmail.com";
-        String password = "Password : ******";
+        Cursor cursor = dbHelper.showconnectedaccount();
+        cursor.moveToFirst();
+        String username = "Username : " +cursor.getString(1);
+        String email = "E-mail : " + cursor.getString(2);
+        String password = "Password : " + cursor.getString(3);
+        String Imageavatar = cursor.getString(4);
+
         String cookie = "Cookie : ";
         String avatar = "Avatar : ";
 
@@ -35,5 +50,19 @@ public class account_show extends AppCompatActivity {
 
         TextView avatarShow = findViewById(R.id.avatarShow);
         avatarShow.setText(avatar + "\n");
+
+        ImageView imageAvatar = findViewById(R.id.avatarImg);
+        imageAvatar.setImageBitmap(stringtobitmap(Imageavatar));
+
+    }
+    private Bitmap stringtobitmap(String avatar){
+        Bitmap bit = null ;
+        try {
+            byte[] decodeString = Base64.decode(avatar , Base64.DEFAULT) ;
+            bit = BitmapFactory.decodeByteArray(decodeString , 0 , decodeString.length);
+        }catch(Exception e){
+            Log.d("Exception", "image error");
+        }
+        return bit ;
     }
 }
