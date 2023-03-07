cef580ba8e9dd6dd54dc4b7fc520252a945b1207
Fl041 <92439423+Fl041@users.noreply.github.com>
2023-03-07 17:35:33 +0100

    message alerte

--- app/src/main/java/com/example/myapplication/jeu.java
+++ app/src/main/java/com/example/myapplication/jeu.java
@@ -38,14 +38,12 @@ public class jeu extends AppCompatActivity {
         // Afficher le fragment upgrades après avoir cliqué sur le bouton
         Button upgradesBtn = findViewById(R.id.upgradesBtn);
         FragmentContainerView upgradesFragment = findViewById(R.id.upgradesFragment);
-
         upgradesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 upgradesFragment.setVisibility(View.VISIBLE);
             }
         });
-
         if(dbHelper.isconnected()){
             Intent ServiceCookie = new Intent(this, com.example.myapplication.ServiceCookie.class);
             startService(ServiceCookie);
@@ -62,7 +60,7 @@ public class jeu extends AppCompatActivity {
             final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
             executor.scheduleAtFixedRate(task, 0, 1, TimeUnit.SECONDS);
 
-            cookie.setOnClickListener(new View.OnClickListener() {
+           cookie.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Cursor cursor = dbHelper.showconnectedaccount() ;
@@ -92,11 +90,12 @@ public class jeu extends AppCompatActivity {
                 }
             });
         }
+
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()  == android.R.id.home){
+        if (item.getItemId() == android.R.id.home) {
             Intent intent = new Intent(getApplicationContext(), accueil.class);
             startActivity(intent);
         }
